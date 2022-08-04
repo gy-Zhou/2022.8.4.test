d51a783a8dee633c374d44e045b12ade87cb81ad
zhou <zhou_guiyue7@hotmail.com>
2022-08-04 21:00:42 +0800

    new file hello.java

---
+++ src/main/java/com/zhou/gitstudy/Hello.java
@@ -0,0 +1,14 @@
+package com.zhou.gitstudy;
+
+/**
+ * @Description:
+ * @Author: zhou
+ * @Date: 2022/8/4 20:59
+ * @Version: 1.0
+ * @Project: gitStudy
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, world !");
+    }
+}
